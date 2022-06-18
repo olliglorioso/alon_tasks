e7a2417989e954c9b3b21f678caf13c2b387b4df
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-18 15:17:41 +0000

    collecting numbers pt1

---
+++ collecting_numbers_/collecting_numbers.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iterator>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename S>
+ostream& operator<<(ostream& os,
+                    const vector<S>& vector)
+{
+    // Printing all the elements
+    // using <<
+    for (auto element : vector) {
+        os << element << " ";
+    }
+    return os;
+}
+
+int main() {
+    int64_t n;
+    cin >> n;
+    vector<int64_t> a;
+    for (int64_t i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    int64_t moves_amount = 0;
+    int64_t biggest_num = 0;
+    while (true)
+    {
+        for (size_t i = 0; i < a.size(); i++) {
+            if (a[i] > biggest_num) {
+                a.erase(a.begin() + i);
+            }
+        }
+        moves_amount += 1;
+        if (a.size() == 0) {
+            break;
+        }
+    }   
+    
+    
+    cout << moves_amount;
+}
