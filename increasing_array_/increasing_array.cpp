6847a0bbb1b6d802f792a3c3db7595e8d6743f91
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2022-06-18 12:05:43 +0000

    increasing arrays pt7

--- increasing_array_/increasing_array.cpp
+++ increasing_array_/increasing_array.cpp
@@ -25,10 +25,10 @@ int main() {
         cin >> x;
         a.push_back(x);
     }
-    int moves_amount = 0;
+    int64_t moves_amount = 0;
     for (size_t i = 0; i < a.size(); i++) {
         if (a[i] < a[i - 1]) {
-            int am = a[i - 1] - a[i];
+            int64_t am = a[i - 1] - a[i];
             a[i] += am;
             moves_amount += am;
         }
