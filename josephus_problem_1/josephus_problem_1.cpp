2cc3047957ce0f7b30d33d47cc3221f4dfff32da
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-23 11:49:50 +0000

    josephus problem with queue pt1

--- josephus_problem_1/josephus_problem_1.cpp
+++ josephus_problem_1/josephus_problem_1.cpp
@@ -4,17 +4,43 @@
 #include <iostream>
 #include <chrono>
 #include <map>
+#include <queue>
 using namespace std;
+
+queue<int> q;
+
+void print_vector(vector<int64_t> vec) {
+    cout << endl;
+    cout << "vector elements : ";
+    for (int64_t i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+int print_every_second() {
+    if (q.size() == 0) {
+        return 0;
+    }
+    int front2 = q.front();
+    q.pop();
+    q.push(front2);
+    int front = q.front();
+    q.pop();
+    cout << front << " ";
+    
+    
+    print_every_second();
+}
  
 int main() {
     int n;
     cin >> n;
- 
-    for (int64_t i = 2; i <= n; i += 2) {
-        cout << i << " ";
+    
+    for (int64_t i = 1; i <= n; i++) {
+        q.push(i);
     }
-    for (int64_t i = 1; i <= n; i += 2) {
-        cout << i << " ";
-    }
-    return 0;
+
+    print_every_second();
+    cout << endl;
 }
