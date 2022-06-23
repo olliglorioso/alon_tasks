845a0259144b0160a32c8fefea108b203b260739
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-23 09:30:34 +0000

    Make the algorithm simpler and sort by END TIME.

--- movie_festival_/movie_festival.cpp
+++ movie_festival_/movie_festival.cpp
@@ -5,41 +5,29 @@
 #include <chrono>
 #include <map>
 using namespace std;
-
+ 
 int main() {
     int movie_amount;
     cin >> movie_amount;
     vector<pair<int, int>> times;
-
+ 
     for (int i = 1; i <= movie_amount; i++) {
         // Get two numbers in one line as input
         int start_time, end_time;
         cin >> start_time >> end_time;
-        times.push_back(make_pair(start_time, end_time));
+        times.push_back(make_pair(end_time, start_time));
     }
-
+ 
     sort (times.begin(), times.end());
-    // Get the shortest movies for each start time.
-    vector<pair<int, int>> movie_times;
-    movie_times.push_back(times[0]);
-
-    for (int i = 1; i < times.size(); i++) {
-        int n = movie_times.size();
-        if ((times[i].first == movie_times[n - 1].first) && ((times[i].second - times[i].first) < (movie_times[n - 1].second - movie_times[n - 1].first))) {
-            movie_times[n - 1] = times[i];
-        } else {
-            movie_times.push_back(times[i]);
-        }
-
-    }
     
     int watched_movies = 0;
     int current_time = 0;
-    for (int i = 0; i < times.size(); i++) {
-        if (times[i].first >= current_time) {
+    for (int i = 0; i < movie_amount; i++) {
+        if (times[i].second > current_time) {
             watched_movies++;
-            current_time = times[i].second;
+            current_time = times[i].first;
         }
     }
+
     cout << watched_movies << endl;
 }
