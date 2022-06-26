92e7ad52f2f1d128de9f80b2c3f163be2360abad
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-26 16:59:23 +0000

    josephus problem II pt3

--- josephus_problem_2_/josephus_problem_2.cpp
+++ josephus_problem_2_/josephus_problem_2.cpp
@@ -8,33 +8,42 @@
 #include <tuple>
 using namespace std;
 
-queue<int> q;
+queue<int32_t> q;
+bool isOver = false;
 
-int print_every_second(int k) {
+int32_t print_every_second(int32_t k, int32_t curLen) {
     if (q.size() == 0) {
         return 0;
     }
-    for (int i = 1; i <= k; i++) {
-        int fronttieri = q.front();
+    int32_t s;
+    if (isOver == true) {
+        s = k % curLen;
+        curLen -= 1;
+    }
+
+    for (int32_t i = 1; i <= s; i++) {
+        int32_t fronttieri = q.front();
         q.pop();
         q.push(fronttieri);
     }
-    int front = q.front();
+    int32_t front = q.front();
     q.pop();
     cout << front << " ";
     
-    
-    print_every_second(k);
+    print_every_second(k, curLen);
 }
 
 int main() {
-    int n, k;
+    int32_t n, k;
     cin >> n >> k;
 
-    for (int i = 1; i <= n; i++) {
+    for (int32_t i = 1; i <= n; i++) {
         q.push(i);
     }
+    if (k > n) {
+        isOver = true;
+    }
 
-    print_every_second(k);
+    print_every_second(k, n);
     return 0;
 }
