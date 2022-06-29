24d23d10417f0bf8efac844bd2d31b127005e939
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-29 09:39:27 +0000

    two sets pt5

--- two_sets_2_/two_sets_2.cpp
+++ two_sets_2_/two_sets_2.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <string>;
 #include <iterator>
 #include <vector>
 #include <iostream>
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-int check_if_eligible(int n) {
+long check_if_eligible(long n) {
     // Lukujen 1...n summa voidaan laskea kaavalla (n*(n+1))/2. Jos summa on pariton, vastaus on NO.
-    int sum = (n * (n + 1)) / 2;
+    long sum = (n * (n + 1)) / 2;
     if (sum % 2 != 0) {
         cout << 0;
         return -1;
@@ -19,39 +19,27 @@ int check_if_eligible(int n) {
 }
 
 int main() {
-    int n;
+    long n;
     cin >> n;
-    int modulo = pow(10, 9) + 7;
-    int sum = check_if_eligible(n);
+    long modulo = pow(10, 9) + 7;
+    long sum = check_if_eligible(n);
     if (sum == -1) return 0;
-    int answer = sum / 2;
+    long answer = sum / 2;
     
-    vector<int> numbers;
-    for (int i = 1; i <= n; i++) {
-        numbers.push_back(i);
-    }
-
-    
-    vector<vector<int>> sums(n,vector<int>(answer+1));
+    vector<vector<long>> sums(n,vector<long>(answer+1));
     sums[0][0] = 1;
 
-    for (int a = 1; a < n; a++) {
-        for (int b = 0; b <= answer; b++) {
+    for (long a = 1; a < n; a++) {
+        for (long b = 0; b <= answer; b++) {
             // Two possibilities: either we include a in the sum or not.
             // 1. We don't include it, and thus 
-            int notIncluded = sums[a-1][b];
-            // 1. We included it, and thus
-            int included = sums[a-1][b-a];
-            int total = notIncluded;
-            if (b - a >= 0) {
-                total = included + notIncluded;
-                total %= modulo;
-            } else {
-                total %= modulo;
+            long total = sums[a-1][b];
+            long rem = b - a;
+            if (rem >= 0) {
+                total += sums[a-1][rem];
             }
-            sums[a][b] = total;
+            sums[a][b] = (total %= modulo);
         }
     }
     cout << sums[n-1][answer] << endl;
-    return 0;
 }
