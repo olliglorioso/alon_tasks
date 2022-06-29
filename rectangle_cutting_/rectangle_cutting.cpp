b9a7f6ec3b2dfb30203e51b6552c3aa709989ada
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-29 20:59:36 +0000

    rectangle cutting

--- rectangle_cutting_/rectangle_cutting.cpp
+++ rectangle_cutting_/rectangle_cutting.cpp
@@ -5,24 +5,33 @@
 
 using namespace std;
 
-int moves = 0;
 
-int cut_into_squares(int height, int width) {
-    if (height == width) {
-        return moves;
-    } else if (height > width) {
-        moves++;
-        cut_into_squares(height - width, width);
-    } else if (width > height) {
-        moves++;
-        cut_into_squares(height, width - height);
+long long cut_into_squares(long long height, long long width) {
+    int values[height + 1][width + 1];
+
+    for (int a = 1; a <= height; a++) {
+        for (int b = 1; b <= width; b++) {
+            if (a == b) {
+                values[a][b] = 0;
+            } else {
+                int value = 100000;
+                for (int i = 1; i < b; i++) {
+                    value = min(value, values[a][b - i] + values[a][i] + 1);
+                }
+                for (int i = 1; i < a; i++) {
+                    value = min(value, values[a - i][b] + values[i][b] + 1);
+                }
+                values[a][b] = value;
+            }
+        }
     }
+    return values[height][width];
 }
 
 int main () {
-    int leveys, korkeus;
+    long long leveys, korkeus;
     cin >> leveys >> korkeus;
 
-    int moveAmount = cut_into_squares(leveys, korkeus);
+    long long moveAmount = cut_into_squares(leveys, korkeus);
     cout << moveAmount << endl;
-}
+} 
