6e2b8d13621b6b0eb2e44e3f3179facb5b4e1e8f
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-29 08:03:09 +0000

    two sets pt4

--- two_sets_2_/two_sets_2.cpp
+++ two_sets_2_/two_sets_2.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-long long check_if_eligible(long long n) {
+int check_if_eligible(int n) {
     // Lukujen 1...n summa voidaan laskea kaavalla (n*(n+1))/2. Jos summa on pariton, vastaus on NO.
-    long long sum = (n * (n + 1)) / 2;
+    int sum = (n * (n + 1)) / 2;
     if (sum % 2 != 0) {
         cout << 0;
         return -1;
@@ -17,32 +17,32 @@ long long check_if_eligible(long long n) {
         return sum;
     }
 }
-vector<vector<long long>> sums;
+
 int main() {
-    long long n;
+    int n;
     cin >> n;
-    long long modulo = pow(10, 9) + 7;
-    long long sum = check_if_eligible(n);
+    int modulo = pow(10, 9) + 7;
+    int sum = check_if_eligible(n);
     if (sum == -1) return 0;
-    long long answer = sum / 2;
+    int answer = sum / 2;
     
-    vector<long long> numbers;
-    for (long long i = 1; i <= n; i++) {
+    vector<int> numbers;
+    for (int i = 1; i <= n; i++) {
         numbers.push_back(i);
     }
 
     
-    sums.resize(n,vector<long long>(answer+1,0));
+    vector<vector<int>> sums(n,vector<int>(answer+1));
     sums[0][0] = 1;
 
-    for (long long a = 1; a < n; a++) {
-        for (long long b = 0; b <= answer; b++) {
+    for (int a = 1; a < n; a++) {
+        for (int b = 0; b <= answer; b++) {
             // Two possibilities: either we include a in the sum or not.
             // 1. We don't include it, and thus 
-            long long notIncluded = sums[a-1][b];
+            int notIncluded = sums[a-1][b];
             // 1. We included it, and thus
-            long long included = sums[a-1][b-a];
-            long long total = notIncluded;
+            int included = sums[a-1][b-a];
+            int total = notIncluded;
             if (b - a >= 0) {
                 total = included + notIncluded;
                 total %= modulo;
